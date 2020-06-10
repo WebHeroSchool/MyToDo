73024a1116ce4c04abb75aba3f265c4179e1f336
snypoon <snypoon2202@gmail.com>
2020-06-10 17:26:49 +0300

    task complete

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import './Input.css';
 
-const Input = () => (<input placeholder='Введите новое дело'></input>);
+const CssTextField = withStyles({
+  root: {
+    '& .MuiFormControl-root': {
+      'width': '100%'
+    },
+    '& .MuiFormLabel-root': {
+      'font-family': 'Ubuntu',
+    },
+    '& .MuiFormLabel-root.Mui-focused': {
+      color: '#2ECFC1'
+    },
+    '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+      'border-color': '#2ECFC1'
+    },
+    '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline':{
+      'border-color': '#2ECFC1'
+    }
+  },
+})(TextField);
+
+const Input = () => (<CssTextField id="outlined-basic" label="новое дело" variant="outlined" />);
 
 export default Input;
