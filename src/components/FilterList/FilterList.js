65170e3ef9268d79bb5cba12f8b82a14fc0b8f57
snypoon <snypoon2202@gmail.com>
2020-06-15 13:47:04 +0300

    task complete and fix keys

--- src/components/FilterList/FilterList.js
+++ src/components/FilterList/FilterList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import style from './FilterList.module.css'
 import FilterItem from '../FilterItem/FilterItem'
 
-const Filter = ({filterItems}) => (<ul key={filterItems.item} className={style.list}>
-  {filterItems.map(item =>
-    <FilterItem item={item.item} active={item.active}/>
+const Filter = ({filterItems}) => (<ul className={style.list}>
+  {filterItems.map((item, key) =>
+    <FilterItem key={key} item={item.item} active={item.active}/>
   )}
 </ul>)
 
