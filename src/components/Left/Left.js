a0f84f926d25060e0d5fd45d93bb0b6060e46db0
snypoon <snypoon2202@gmail.com>
2020-06-17 17:26:42 +0300

    task coplete

--- src/components/Left/Left.js
+++ src/components/Left/Left.js
@@ -3,4 +3,8 @@ import styles from './Left.module.css';
 
 const Left = ({count}) => (<div>Осталось выполнить : <span className={styles.num}>{count}</span></div>);
 
+Left.defaultProps = {
+  count: 0
+};
+
 export default Left;
