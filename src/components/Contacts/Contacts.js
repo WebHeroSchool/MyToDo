f37fc89a39c422f36044afe25c03f4bd70e6925b
snypoon <snypoon2202@gmail.com>
2020-06-24 16:09:15 +0300

    task complete

---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contacts = () => (
+  <div></div>
+)
+
+export default Contacts; 
---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,93 @@
+import React, {useState} from 'react';
+import ItemList from '../ItemList/ItemList';
+import Left from '../Left/Left';
+import Input from '../Input/Input';
+import Filter from '../FilterList/FilterList';
+import Clear from '../Clear/Clear';
+import styles from './Todo.module.css'
+
+const Todo = () => {
+  const initialState = {
+    todoItems:[{
+      value: 'важное дело',
+      done: false,
+      id: 1
+    },
+    {
+      value: 'дело',
+      done: true,
+      id: 2
+    },
+    {
+      value: 'еще одно дело',
+      done: true,
+      id: 3
+    }],
+    
+    filterItems: [{
+      item: 'Все',
+      active: true
+    },
+    {
+      item: 'Активные',
+      active: false
+    },
+    {
+      item: 'Выполненые',
+      active: false
+    }],
+    
+    count: 3 
+  }
+
+  const [todoItems, setTodoItems] = useState(initialState.todoItems);
+  const [filterItems] = useState(initialState.filterItems);
+  const [count, setCount] = useState(initialState.count);
+
+  const checkboxChange = id => {
+    const newTodoItems = todoItems.map(item => {
+      const newItem = { ...item };
+      if(item.id === id){
+        newItem.done = !newItem.done;
+      }
+      return newItem;
+    })
+    setTodoItems(newTodoItems);
+  }
+
+  const onClickDelete = id => {
+    const newTodoItems = todoItems.filter(item => item.id !== id)
+    setTodoItems(newTodoItems);
+  }
+
+  const onKeyDownAdd = value => {
+    const newTodoItems = [
+    ...todoItems,
+    {
+      value: value,
+      done: false,
+      id: count +1
+    }];
+    setTodoItems(newTodoItems);
+    setCount(count +1);
+  };
+
+  
+  return(<>
+      <h1 className={styles.title}>M<span className={styles.title_color}>y</span>ToDo</h1>
+      <div className={styles.content}>
+        <Input onKeyDownAdd={onKeyDownAdd}/>
+        <ItemList todoItems={todoItems} 
+                  checkboxChange={checkboxChange} 
+                  onClickDelete={onClickDelete}
+        />
+      </div>
+      <div className={styles.footer}>
+        <Left count={todoItems.length}/>
+        <Filter filterItems={filterItems}/>
+        <Clear />
+      </div>
+    </>)
+}
+
+export default Todo;
